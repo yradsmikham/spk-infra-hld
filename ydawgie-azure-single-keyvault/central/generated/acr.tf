8c7f9cad2068329409207bd3065587075d30539b
Yvonne Radsmikham <yvonne.radsmikham@gmail.com>
2020-01-13 15:58:32 -0800

    clean up

--- ydawgie-azure-single-keyvault/central/generated/acr.tf
+++
@@ -1,7 +0,0 @@
-module "acr" {
-  source = "github.com/microsoft/bedrock?ref=master//cluster/azure/acr"
-
-  acr_name                 = "${var.acr_name}"
-  enable_acr               = "${var.enable_acr}"
-  resource_group_name      = "${data.azurerm_resource_group.cluster_rg.name}"
-}
--- ydawgie-azure-single-keyvault/east/generated/backend.tfvars
+++
@@ -1,4 +0,0 @@
-storage_account_name="ydawgie"
-access_key="WxAL04KFQHRGm+HJZLpxjLIfWLhFgyzOlVsB/eiuNAZ4lOv/hLzyfnmCIcgHZuXn2znupqndpNfVlbansNlKrw=="
-container_name="ydawgie"
-key="ydawgie-single-kv-east-tfstate"
